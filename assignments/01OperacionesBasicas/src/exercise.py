bfbb2865a93803fd7f4776217a156d064fd7b7ca
shanimucar <88851097+shanimucar@users.noreply.github.com>
2021-08-12 20:56:14 -0500

    Update exercise.py

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -5,9 +5,9 @@ number1 = int(input()) #Read the first value
 print('Enter a second number')
 number2 = int(input()) #Read the second value
 
-suma = number1 + number2
-resta = number1 - number2
-multi = number1 * number2
+suma=number1+number2
+resta=number1-number2
+multi=number1*number2
 
 print('The addition of these numbers is: ' + str(suma)) #Show the addition
 print('Your substraction of these numbers is: ' + str(resta)) #Show the substraction
