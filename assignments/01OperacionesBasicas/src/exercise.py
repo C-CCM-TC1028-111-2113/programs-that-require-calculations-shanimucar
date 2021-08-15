2f0ead2c2d4fa5ae565b6c356d811a4bfeba7b6b
shanimucar <88851097+shanimucar@users.noreply.github.com>
2021-08-14 21:36:59 -0500

    Update exercise.py

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,19 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
 
-    print('Enter a number')
-num1 = int(input() ) #Read the first value
+print('Enter a number')
+number1 = int(input()) #Read the first value
 print('Enter a second number')
-num2 = int(input() ) #Read the second value
+number2 = int(input()) #Read the second value
 
-suma = num1 + num2
-resta = num1 - num2
-multi = num1 * num2
+suma = number1 + number2
+resta = number1 - number2
+multi = number1 * number2
 
 print('The addition of these numbers is: ' + str(suma)) #Show the addition
 print('Your substraction of these numbers is: ' + str(resta)) #Show the substraction
 print('Your multiplication of these numbers is: ' + str(multi)) #Show the multiplication
-
    
 
 if __name__ == '__main__':
