a7b10601f76a74867e065374e2fac1e4d591ca91
shanimucar <88851097+shanimucar@users.noreply.github.com>
2021-08-15 14:16:43 -0500

    Update exercise.py

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -5,7 +5,7 @@ def main():
     calif2=float(input("Calificación de la materia: "))
     calif3=float(input("Calificación de la materia: "))
     calif4=float(input("Calificación de la materia: "))
-    promedio = (c1+c2+c3+c4)/4
+    promedio = (calif1+calif2+calif3+calif4)/4
     print ("El promedio es:",promedio)
 
 
