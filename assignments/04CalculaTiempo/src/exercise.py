83412da1991b7f8083164e74de5beddb99a25f6a
shanimucar <88851097+shanimucar@users.noreply.github.com>
2021-08-15 14:42:56 -0500

    Update exercise.py

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -2,8 +2,8 @@ def main():
     #escribe tu código abajo de esta línea
     edad = int(input("Dame tu edad: "))
     anoActual = int(input("Dame el año actual: "))
-    anosFaltantes=100-edad
-    tiempo=anoActual+anosFaltantes
+    anosFaltantes = 100 - edad
+    tiempo = anoActual + anosFaltantes
     print("Cumplirás 100 años en el año: ",tiempo)
 
 
