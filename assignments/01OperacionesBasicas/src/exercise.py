f749de0787115dec4892518b430e3694e7ef2396
shanimucar <88851097+shanimucar@users.noreply.github.com>
2021-08-15 11:23:12 -0500

    Update exercise.py

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -2,13 +2,13 @@ def main():
     #escribe tu código abajo de esta línea
 
     print('Dame un número')
-num1 = int(input()) #Read the first value
-print('Dame otro número')
-num2 = int(input()) #Read the second value
+number1 = int(input()) #Read the first value
+print('Dame un número')
+number2 = int(input()) #Read the second value
 
-suma = num1 + num2
-resta = num1 - num2
-multi = num1 * num2
+suma = number1 + number2
+resta = number1 - number2
+multi = number1 * number2
 
 print('Suma: ' + str(suma)) #Show the addition
 print('Resta: ' + str(resta)) #Show the substraction
