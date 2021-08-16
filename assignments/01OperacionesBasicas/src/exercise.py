5d252898bc78c3939f90dd6f47ce04b83f0d4963
shanimucar <88851097+shanimucar@users.noreply.github.com>
2021-08-16 07:01:10 -0500

    Update exercise.py

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,16 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
 
-    n1 = integer(input("Dame un número: "))
-    n2 = integer(input("Dame un número: "))
+    n1 = int(input("Dame un número:"))
+    n2 = int(input("Dame un número:"))
 
 suma = n1 + n2
 resta = n1 - n2
 multi = n1 * n2
 
-print('Suma: ', suma) #Show the addition
-print('Resta: ', resta) #Show the substraction
-print('Multiplicación: ', multi) #Show the multiplication
+print("Suma:", suma) #Show the addition
+print("Resta:", resta) #Show the substraction
+print("Multiplicación:", multi) #Show the multiplication
    
 
 if __name__ == '__main__':
